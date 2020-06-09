d94cbf050dd6537c3cb9d79a2a27b3b8453b630b
Marek Kancir <kancir.marek@gmail.com>
2020-06-09 11:43:51 +0200

    store z's from activation

--- bru_net/bru_regressor.py
+++ bru_net/bru_regressor.py
@@ -27,28 +27,31 @@ class BRU_Regressor():
         if not with_activation:
             return np.dot(a, w.T)
         z = np.dot(a, w.T)
-        return self.activation(z)
+        return self.activation(z), z
     
     def add_bias(self, x):
         return np.c_[np.ones((x.shape[0],1)), x]
     
     def forward_prop(self, x):
+        z = {}
         x = self.add_bias(x)
-        a = self.step_forward(x, self.w[0])
+        a, _z = self.step_forward(x, self.w[0])
+        z.update({0: _z})
         a = self.add_bias(a)
         for layer in range(1, self.hidden_layers+2):
             if layer!=self.hidden_layers+1:
-                a = self.step_forward(a, self.w[layer])
+                a, _z = self.step_forward(a, self.w[layer])
+                z.update({layer: _z})
                 a = self.add_bias(a)
             else:
                 a = self.step_forward(a, self.w[layer], with_activation=False) # for output layer only linear combination
-        return a
+        return a,z
 # =============================================================================
 #   do fitu a podobne daj na np.float32 
 # =============================================================================
 if __name__=='__main__':
-    reg = BRU_Regressor(input_shape=(1,4), n_hidden_layers=2)
-    res = reg.forward_prop(np.random.rand(1,4))
+    reg = BRU_Regressor(input_shape=(1,4), n_hidden_layers=3)
+    res, z = reg.forward_prop(np.random.rand(1,4))
     print(res)
 
 
